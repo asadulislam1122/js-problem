47fc1e170ba744b28678e245175acb127d4b9001
asadulislam <asadulislam0288@gmail.com>
2026-04-10 21:33:41 +0600

    first commit

---
+++ numbar.js
@@ -0,0 +1,18 @@
+// check even number
+
+function checkEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  }
+}
+
+console.log(checkEven(6));
+
+// check old number
+
+function checkOld(num) {
+  if (num % 2 !== 0) {
+    return true;
+  }
+}
+console.log(checkOld(3));
---
+++ revarseStirng.js
@@ -0,0 +1,5 @@
+function revarceString(str) {
+  return (result = str.split("").reverse().join(""));
+}
+
+console.log(revarceString("Hello World"));
