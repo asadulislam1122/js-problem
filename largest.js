0d5d7335f8e8f9ebc57cb6ad90b9dfa32cc72639
asadulislam <asadulislam0288@gmail.com>
2026-04-11 21:54:58 +0600

    added by largest number

---
+++ largest.js
@@ -0,0 +1,27 @@
+// function findLargest(numbers) {
+//   let largest = numbers[0];
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > largest) {
+//       largest = numbers[i];
+//     }
+//   }
+//   return largest;
+// }
+
+// console.log(findLargest([1, 2, 3, 4, 5]));
+
+// const findlargest = (arr) => Math.max(...arr);
+
+// console.log(findlargest([22, 44]));
+
+function findlargest(arr) {
+  let largest = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+console.log(findlargest([22, 44, 77, 99]));
--- sum.js
+++ sum.js
@@ -1,13 +1,29 @@
 // const sum = (a, b) => a + b;
 // console.log(sum(1, 2));
 
-function sumArry(arr) {
-  let total = 0;
+// function sumArry(arr) {
+//   let total = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    total += arr[i];
-  }
-  return total;
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//   }
+//   return total;
+// }
+
+// console.log(sumArry([1, 2, 3, 4]));
+
+// function sumArray(arr) {
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//   }
+//   return total;
+// }
+
+// console.log(sumArray([1, 2, 3, 4, 5]));
+
+function sum(a, b) {
+  return a + b;
 }
 
-console.log(sumArry([1, 2, 3, 4]));
+console.log(sum(600, 2));
